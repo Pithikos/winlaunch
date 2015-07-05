5febbcaac8c17ebf7c8fd5ccfcd6ce57b0a8d79d
Pithikos <manos@gmail.com>
2015-07-05 13:51:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# xdo
+
+A small library that let's you move around, resize, switch between desktops, windows. The library depends on external X tools like xdotool, xwininfo, xprop, etc. These tools are in most cases already installed!
+
+## Dependencies
+
+sudo apt-get install xdotool xprop xwininfo
+
+## API
+
+| Function                     | Description                                                                                                                                      |   |
+|------------------------------|--------------------------------------------------------------------------------------------------------------------------------------------------|---|
+| xrun(command)                | Runs a GUI application. The window id (wid) is returned and the process id (pid) that the window belongs to.                           |   |
+| win_size(wid, width, height) | Resizes the window with id `wid` to given width and height values. If no width and height are given, the current size of the window is returned. |   |
+| win_pos(wid, x, y)           |                                                                                                                                                  |   |
+| win_desktop(wid, desktop)    | Sets the window with id `wid` to the given desktop. If desktop is not given the current desktop of the window is returned.                       |   |
+| win_screen(wid)      | Gives the current screen where the window with id `wid` is.                          |   |
---
+++ xdo.py
@@ -0,0 +1,100 @@
+'''
+TODO:
+	* Allow to pass logname to xrun.
+'''
+import subprocess
+from time import sleep
+import re
+
+
+
+# ------------------------- Starting processes -------------------------
+
+def run_cmd(cmd):
+	''' Command to run separated by blank spaces '''
+	proc = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE,
+	                                        stderr=subprocess.PIPE)
+	return proc
+	
+def get_proc_output(proc):
+	out, err = proc.communicate()
+	return out.decode(), err.decode()
+
+def get_cmd_output(cmd):
+	return get_proc_output(run_cmd(cmd))
+
+def xrun(cmd):
+	''' Run a gui application '''
+	open_windows = current_windows()
+	proc = run_cmd(cmd)
+
+	# Wait for new proc to open GUI
+	while (open_windows == current_windows()):
+		sleep(0.2)
+
+	new_wids = [wid for wid in current_windows() if wid not in open_windows]
+	if len(new_wids) > 1:
+		print('ERROR: xrun(): too many window ids found')
+		return None
+	if len(new_wids) < 1:
+		print('ERROR: xrun(): too few window ids found')
+		return None
+	wid = new_wids[0]
+	pid = win_pid(wid)
+	return wid, pid
+
+
+
+
+# -------------------------------- X Win -------------------------------
+
+def xdo(do):
+	out, err = get_cmd_output('xdotool ' + do)
+	if err:
+		print('ERROR: %s' % err)
+	return out
+
+def win_pid(wid):
+	''' Gives the PID of the process that win id belongs to '''
+	return xdo('getwindowpid %s' % wid).strip()
+
+def current_windows():
+	out, err = get_cmd_output('xprop -root')
+	match = re.search(r'_NET_CLIENT_LIST_STACKING\(WINDOW\): window id # (.*)', out)
+	if not match:
+		return None
+	return match.group(1).split(', ')
+
+def win_size(wid, x=None, y=None):
+	if not x or not y:
+		out = xdo('getwindowgeometry %s' % wid)
+		match = re.search(r'Geometry: (.*)', out)
+		if not match:
+			return None
+		return match.group(1).split('x')
+	else:
+		xdo('windowsize %s %s %s' % (wid, x, y))
+
+def win_pos(wid, x=None, y=None):
+	if not x or not y:
+		out = xdo('getwindowgeometry %s' % wid)
+		match = re.search(r'Position: (\d*,\d*)', out)
+		if not match:
+			return None
+		return match.group(1).split(',')
+	else:
+		xdo('windowmove %s %s %s' % (wid, x, y))
+
+def win_screen(wid):
+	out = xdo('getwindowgeometry %s' % wid)
+	match = re.search(r'Position: \d*,\d* \(screen: (\d*)\)', out)
+	if not match:
+		return None
+	return match.group(1)
+
+def win_desktop(wid, desktop=None):
+	''' Gives the desktop number of the given window '''
+	if desktop is None:
+		return xdo('get_desktop_for_window %s' % wid).strip()
+	else:
+		return xdo('set_desktop_for_window %s %desktop' % (wid, desktop))
